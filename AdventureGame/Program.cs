c8449e1f5a24014562cf37c1773555971dcaa553
Jacquee Neal <janeal@aperturecvo.com>
2019-05-08 16:42:02 -0400

    Add project files.

---
+++ AdventureGame/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AdventureGame
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Quest to Enlightenment");
+            Console.WriteLine("A Journey of Illumination");
+            Console.ReadKey();
+        }
+    }
+}
